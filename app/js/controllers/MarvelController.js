8e0514dbae97ac3017d8b89a711ff6462cdcab56
Miguelangel Palma <mangel.2711@gmail.com>
2017-01-13 20:29:36 -0400

    header

--- app/js/controllers/MarvelController.js
+++ app/js/controllers/MarvelController.js
@@ -1,5 +1,17 @@
 App.controller("MarvelController", function ($scope, $rootScope){
 
-	$scope.saludo = "XD"
+	$scope.hola = "XD";
+
+	$scope.bool = true;
+	console.log($scope.bool);
+
+	$scope.onSearch = function(){
+		if($scope.bool=true){
+			$scope.bool=false;
+		}else{
+			$scope.bool=true;
+		}
+	}
+	
 	
 });
