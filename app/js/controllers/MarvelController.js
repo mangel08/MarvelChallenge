a9f45525644f464cb047cb09d0c90313f8e7af0a
Miguelangel Palma <mangel.2711@gmail.com>
2017-01-15 00:26:28 -0400

    maquetacion

--- app/js/controllers/MarvelController.js
+++ app/js/controllers/MarvelController.js
@@ -1,17 +1,11 @@
 App.controller("MarvelController", function ($scope, $rootScope){
+		
+ 
+	$scope.sort = "Sort";
 
-	$scope.hola = "XD";
+	alert("hola");
 
-	$scope.bool = true;
-	console.log($scope.bool);
-
-	$scope.onSearch = function(){
-		if($scope.bool=true){
-			$scope.bool=false;
-		}else{
-			$scope.bool=true;
-		}
-	}
+	
 	
 	
 });
--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,32 @@
 var App  = angular.module("MarvelChallenge", [
-	"ngRoute",
 	"ngResource",
-	"ui.materialize"
+	"ui.materialize",
+	"oc.lazyLoad",
+	"ngRoute"
 	]);
+
+App.config(function($routeProvider){
+
+	$routeProvider
+		.when("/",{
+			controller: "MarvelController",
+			templateUrl: "app/views/home.html"
+		})
+});
+
+App.controller("HeaderController", function($scope, $rootScope){
+
+	$scope.bool = true;
+	// alert($scope.bool);
+
+	$scope.onSearch = function(){
+		if($scope.bool=true){
+			$scope.bool=false;
+		}else{
+			$scope.bool=true;
+		}
+	}
+
+});
+
 
